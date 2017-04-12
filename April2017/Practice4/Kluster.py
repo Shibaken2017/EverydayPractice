7697f17baf5cc59bf71c72bb7da7c5d786fb4163
shibata <kentaro.shibata@datasection.co.jp>
2017-04-12 19:29:02 +0900

    klusterにelementのリストを足すメソッドを追加

--- April2017/Practice4/Kluster.py
+++ April2017/Practice4/Kluster.py
@@ -7,18 +7,27 @@ import numpy as np
 from April2017.Practice4 import Element
 
 class Kluster:
+
     def __init__(self,id,ele_list):
+        '''
+        :type id: int
+        :param id:
+        :type ele_list: list[ Element ]
+        :param ele_list:
+        '''
         '''
 
         :param id: クラスタid
         :param ele_set: elementのset
 
         '''
+
         if len(ele_list)==0 or ele_list==None:
             raise Exception("ele_listが空です")
         self.id=id
         self.ele_list=ele_list
         self.mean=None
+
     def calc_kluster_tr(self,kluster):
         '''
         kluster間の距離を測る
@@ -69,7 +78,23 @@ class Kluster:
                 self.mean+=self.ele_list[i].position
 
         self.mean=self.mean/len(self.ele_list)
+    def add_elements(self,element_list):
+        '''
+        クラスターにelementを足す
+        :param element_list:
+        :return:
+        '''
+        self.ele_list.extend(element_list)
 
+
+
+    def __str__(self):
+        tmp_list=[]
+        for ele in self.ele_list:
+            tmp_list.append(ele.id)
+
+        output="Klu_id:{},ele_list:{}".format(self.id,tmp_list)
+        return output
 
 
 
@@ -83,7 +108,8 @@ if __name__=='__main__':
 
     element_list=[ele1,ele2]
     k=Kluster(1,element_list)
-    print(k.calc_kluster_tr(k))
-    k.calc_tr()
-    print(k.tr)
+    print(k)
+   # print(k.calc_kluster_tr(k))
+    #k.calc_tr()
+    #print(k.tr)
 
