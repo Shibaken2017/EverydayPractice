88c8cd39828d2121e1a077910584cd4ea06d8012
shibaken <kentaro.shibata@datasection.co.jp>
2017-06-12 14:25:40 +0900

    a function which removes 10% from learning data

--- Udacity/outliers/outlier_cleaner.py
+++ Udacity/outliers/outlier_cleaner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -12,9 +13,34 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
+    print "predictions"
+    predictions=np_list(predictions)
+    net_worths=np_list(net_worths)
     ### your code goes here
+    resid=predictions-net_worths
+    abs_resid=np.abs(resid)
+   # print abs_resid
+   # print abs_resid
+    indexes=np.argsort(abs_resid)
+   # print indexes
+    #print indexes
+    ed=int(0.9*float(len(ages)))
 
-    
+    print "aaaaaaaaaaaaaaaaaaa"
+    print "ed :{}".format(ed)
+    #print indexes
+    remained_indexes=indexes[:ed]
+    #print remained_indexes
+    #print remained_indexes
+    for id in remained_indexes:
+       print id
+       cleaned_data.append((ages[id],net_worths[id],resid[id]))
     return cleaned_data
 
+
+
+def np_list(input_list):
+    output_list=[]
+    for i in input_list:
+        output_list.append(i[0])
+    return np.array(output_list)
