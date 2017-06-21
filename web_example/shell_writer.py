a7fe4dfd8abd7c8449d42a2ec45f5388ec434c33
shibaken <kentaro.shibata@datasection.co.jp>
2017-06-21 19:44:45 +0900

    add lines rewriting nginx.conf etc

--- web_example/shell_writer.py
+++ web_example/shell_writer.py
@@ -15,13 +15,15 @@ class ShellWriter:
     def __init__(self):
         print "nyan"
 
-    def exe(self, jmx_dir, report_dir, log_dir, output_fname):
+    def exe(self, jmx_dir, report_dir, log_dir, output_fname,sleep_time=420):
         '''
           :param jmx_dir:
           :param report_dir:
           :param log_dir:
           :return:
           '''
+        self.__sleep_time=sleep_time
+
         self.__output_txt = ""
         self.__jmx_dir = jmx_dir
         self.__log_dir = log_dir
@@ -31,13 +33,15 @@ class ShellWriter:
         with open(output_fname, "w")as writer:
             writer.write(self.__output_txt)
 
-
     def write_initial_part(self):
-        self.__output_txt += "for process in 1\n"
-        self.__output_txt += "for connection in 8\n"
-        self.__output_txt += "do\n"
 
 
+        self.__output_txt += "for process in 1 2 3;do\n"
+        self.__output_txt += "for connection in 1 8 16 32 64 128 256;do\n"
+        # ssh接続＆connetctionとprocess数変更
+        self.__output_txt += "ssh  "
+        self.__output_txt += "sudo python /home/taro_biwajima/gitproject/Practice/web_example/nginx_rewriter.py /etc/nginx/nginx.conf ${process} ${connection}\n"
+        #self.__output_txt+="exit\n"
     def write_jmeter_part(self):
         # jmeter -n -t my_test.jmx -l log.jtl -e -o report
         jmeter_fnames = os.listdir(self.__jmx_dir)
@@ -50,14 +54,16 @@ class ShellWriter:
                   + jmeter_fname.split(".")[0] + ".jtl"
 
             # base="process{}_connection{}_".format(process,connection)+fname.split(".")[0]
-
-            self.__output_txt += "\tjmeter -n -t {} -l {} -e -o {}\n".format(self.__jmx_dir + jmeter_fname, log, report)
+            self.__output_txt+="\tmkdir "+report+"\n"
+            self.__output_txt += "\t./jmeter -n -t {} -l {} -e -o {}\n".format(self.__jmx_dir + jmeter_fname, log, report)
+            self.__output_txt+="\tsleep {}\n".format(self.__sleep_time)
 
         self.__output_txt += "done\n"
-
+        self.__output_txt+="done\n"
 
 if __name__ == "__main__":
     writer = ShellWriter()
     #    def exe(self, jmx_dir, report_dir, log_dir, output_fname):
 
-    writer.exe("./jmx_files/","./report_dir/","./log_dir/","test.sh")
+    writer.exe("/home/shibaken/IDEetc/apache-jmeter-3.2/bin/jmx_files/","/home/shibaken/IDEetc/apache-jmeter-3.2/bin/report_dir/",
+               "/home/shibaken/IDEetc/apache-jmeter-3.2/bin/log_dir/","test.sh")
